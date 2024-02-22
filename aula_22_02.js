e5fa89afe66e0cc01c958627c0fc451e21b6d0af
AnaCarolina16 <cordeiro.caroline.ana@estudante.ifro.edu.br>
2024-02-22 11:22:01 -0400

    first commit

---
+++ aula_22_02.js
@@ -0,0 +1,22 @@
+//variáveis e tipos de dados 
+var nome='Ana Caroline Cordeiro';
+var idade=16;
+console.log('Olá meu nome é',nome ,'e eu tenho' ,idade,'anos');
+
+//Questão 2
+console.log('socorro'. toUpperCase());
+console.log('me TirA dAqUi'. toLowerCase());
+
+//Questão 3
+var num='13';
+console.log(typeof num);
+
+//Questão 4
+var altura=1.61
+var peso=52.500
+console.log(peso/altura**2);
+
+
+
+
+
