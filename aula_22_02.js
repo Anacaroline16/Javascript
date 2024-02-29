e94c1051a330b9356e2c6ff5a64a95646f1b8b97
AnaCarolina16 <cordeiro.caroline.ana@estudante.ifro.edu.br>
2024-02-29 11:29:24 -0400

    kjnjk

--- aula_22_02.js
+++ aula_22_02.js
@@ -16,7 +16,23 @@ var altura=1.61
 var peso=52.500
 console.log(peso/altura**2);
 
+//quetão 5
+var num=7;
+var num1=8;
+console.log(num+num1);
+console.log(num-num1);
+console.log(num*num1);
+console.log(num/num1);
+ 
+//questão 6
+var idade=16;
+var idade1=18;
+console.log(idade1>=idade);
 
+//Questão 7
+var idade=18
+console.log(idade>=18 ? 'Maior de idade':'Menor de idade');
 
-
-
+//Questão 8
+var num=17
+console.log(idade/2===0 ? 'PAR':'Ímpar');
