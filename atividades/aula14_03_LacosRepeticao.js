725356675beb1c70b0a7728ab64c1333e6f90947
AnaCarolina16 <cordeiro.caroline.ana@estudante.ifro.edu.br>
2024-03-21 11:03:26 -0400

    ...

--- atividades/aula14_03_LacosRepeticao.js
+++ atividades/aula14_03_LacosRepeticao.js
@@ -30,13 +30,84 @@ console.log(mediaTurma)
 */
 //Do While
 
-let num= input.questionInt('Digite um número aleatorio de 1 a 100:');
+/*
+const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+let num;
 do {
-    let min = 1;
-    let max = 100;
-    const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-console.log(numeroAleatorio);
-} while (contador==num);
-    console.log('Você acertou!!')
-    break
-    else:
+    let num= input.questionInt('Digite um número aleatorio de 1 a 100:');
+    if (num===numeroAleatorio){
+        console.log('Você acertou!!');
+        break
+    }else if (num<numeroAleatorio){
+        console.log('Tente um numero maior.');
+    }else{
+        console.log('Tente um numero menor.');
+    }
+    
+} while (num  !== numeroAleatorio);
+*/
+//For
+//questão 1
+/*
+let anterior = 0;
+let atual = 1;
+console.log(anterior);
+console.log(atual);
+for (let i = 3; i <= 20; i++) {
+    let proximo = anterior + atual;
+    console.log(proximo);
+    anterior = atual;
+    atual = proximo;
+}
+*/
+//questao 2
+/*
+for (let i = 1; i <= 10; i++) {
+   
+    let nome = input.question(`Informe o nome da ${i}ª dadosPessoas: `);
+    let salario = input.questionFloat(`Informe o salário de ${nome}: `);
+    let impostoRenda = 0;
+
+    if (salario <= 2100) {
+        impostoRenda = 0;
+    } else if (salario <= 2800) {
+        impostoRenda = salario * 0.075;
+    } else if (salario <= 3750) {
+        impostoRenda = salario * 0.15;
+    } else if (salario <= 4660) {
+        impostoRenda = salario * 0.225;
+    } else {
+        impostoRenda = salario * 0.275;
+    }
+    console.log(`\nImposto de Renda a ser pago`);
+    console.log(`Nome: ${nome} \nImposto de Renda: R$ ${impostoRenda.toFixed(2)}\n`);
+}
+*/
+//for in
+//questão 1
+
+const dadosPessoas = {
+    nome: "Ana",
+    idade: 16,
+    cidade: "Vilhena"
+};
+for (let propriedades in dadosPessoas) {
+    console.log(`${propriedades}: ${dadosPessoas[propriedades]}`);
+}
+// for Of 
+//questão 1
+const cores = ["Rosa", "Azul", "", "Verde", "Vermelho"];
+for (let cor of cores) {
+    console.log(cor);
+}
+
+// 03-Escreva um programa que itere sobre os caracteres de uma string e imprima cada caractere.
+console.log("\n\t--- Exercício 03 ---")
+// Definir uma string
+const string = "Socorro";
+
+// Iterar sobre os caracteres da string usando for...of
+for (let caractere of string) {
+    // Imprimir o caractere
+    console.log(caractere);
+}
