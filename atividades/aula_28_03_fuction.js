c82be9c9207dccea938abad7042f6b9ab0d493b4
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2024-04-10 21:16:41 -0300

    ...

--- atividades/aula_28_03_fuction.js
+++ atividades/aula_28_03_fuction.js
@@ -1,4 +1,4 @@
-import input from 'readline-sync';
+//import input from 'readline-sync';
 /*
 //questao 1
 function simples(letras){
@@ -37,9 +37,53 @@ console.log(soma(10,80));
 */
 
 //questao 4
-function valor(numero, porcentagem){
-    return numero*(porcentagem/100)
-};
-let numero=input('Informe um valor:');
-let porcentagem=input('Informe um valor a ser ca');
-console.log(categoria(2));
+ // function valor(numero, porcentagem){
+ //     return numero*(porcentagem/100)
+ // };
+ // let numero=input('Informe um valor:');
+// let porcentagem=input('Informe o valor da porcentagem:');
+// // console.log(valor(2, 15));
+
+// let valor=(numero, porcentagem)=>{ return numero*(porcentagem/100)
+// }
+// console.log(valor(5,12))
+
+//Exercicio 5
+// function produto(valor,porcentagem){
+//     return valor-(porcentagem/100)
+// }
+// console.log(produto(150,30))
+// let produto=(valor,porcentagem)=>{return valor-(porcentagem/100)}
+// console.log(produto(100,30))
+
+//Nível Médio
+//exercicio 6
+// function area(raio){
+//     return Math.pow(raio,2)*3.14
+// }
+// console.log(area(5))
+// let area= (raio)=>{return Math.pow(raio,2)*3.14}
+// console.log(area(7))
+
+
+
+//exercicio 7
+// function temperatura(celsius){
+//     return(celsius*1.8)+32
+// }
+// console.log(temperatura(38))
+// let temperatura=(celsius)=>{return(celsius*1.8)+32}
+// console.log(temperatura(41))
+
+//exercicio 8
+function imc(altura,peso){
+    return (peso/(altura*altura)).toFixed(2)
+}
+console.log(imc(1.61,52.48))
+
+//Nível Desafiador
+// exercicio 9
+// function senha(nums){
+//     if nums==
+
+// }
